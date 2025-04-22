aa49659eda660b1dfa77b8f0d129a85caf6f182b
Mostafa-123 <me2609314@gmail.com>
2025-04-22 18:23:30 +0200

    update request

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -30,7 +30,17 @@ class UpdateTaskRequest extends FormRequest
             'status' => 'sometimes|in:pending,in_progress,completed',
             'deadline' => 'nullable|date',
             'assign_to' => 'nullable|exists:users,id',
-            'image' => 'nullable|mimes:jpg,jpeg,png,gif,webp|max:2048'
+            'image' => 'nullable|mimes:jpg,jpeg,png|max:2048'
+        ];
+    }
+
+
+    public function messages()
+    {
+        return [
+            'status.in' => 'Status must be one of: pending, in_progress, or completed',
+            'assign_to.exists' => 'The selected user to assign does not exist',
+            'image.mimes' => 'Image must be one of the following types: jpg, jpeg, png',
         ];
     }
 
